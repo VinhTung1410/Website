ad3961d53a43ee781e17ca538f3d450a9abe1e68
Tung <vinhtung1410@gmail.com>
2025-04-21 15:37:12 +0700

    UserRole

--- NewWeb/Areas/Admin/Controllers/UserController.cs
+++ NewWeb/Areas/Admin/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Web.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Web.Utility;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace NewWeb.Areas.Admin.Controllers
 {
@@ -15,16 +16,73 @@ namespace NewWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public UserController(ApplicationDbContext db)
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
             return View();
         }
 
- 
+        public IActionResult RoleManagement(string userid)
+        {
+            var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == userid);
+            if (userRole == null)
+            {
+                // Handle the case where no matching user role is found
+                return NotFound("User role not found.");
+            }
+            string RoleID = userRole.RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userid),
+                RoleList = _db.Roles.Select(i => new SelectListItem()
+                {
+                    Text = i.Name,
+                    Value = i.Id
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem()
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagementVM.ApplicationUser.Role == oldRole))
+            {
+                // a Role was updated
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
         #region API CALLS
 
         [HttpGet]
@@ -37,8 +95,17 @@ namespace NewWeb.Areas.Admin.Controllers
 
             foreach (var user in objUserList)
             {
-                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
-                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                var userRoleEntry = userRole.FirstOrDefault(u => u.UserId == user.Id);
+                if (userRoleEntry != null)
+                {
+                    var roleId = userRoleEntry.RoleId;
+                    user.Role = roles.FirstOrDefault(u => u.Id == roleId)?.Name;
+                }
+                else
+                {
+                    user.Role = "Unknown"; // Handle cases where no role is found  
+                }
+
                 if (user.Company == null)
                 {
                     user.Company = new Company()
@@ -50,7 +117,7 @@ namespace NewWeb.Areas.Admin.Controllers
             return Json(new {data = objUserList });
         }
         [HttpPost]
-        public IActionResult LockUnlock([FromBody]string id)
+        public IActionResult LockUnlock([FromBody] string id)
         {
             var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
             if (objFromDb == null)
